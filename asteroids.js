e5d4cc6f47207dc6e435d9b160112ef2abddc3a2
Jennifer Hamon <jhamon@gmail.com>
2013-10-23 23:37:09 -0700

    Bumpy asteroids.

--- asteroids.js
+++ asteroids.js
@@ -2,12 +2,32 @@
 	var Asteroids = window.Asteroids = (window.Asteroids || {});
 
 	var Asteroid = Asteroids.Asteroid = function(pos, speed, direction, radius, color) {
-		Asteroids.MovingObject.call(this, pos, speed, direction, radius, color)
+		Asteroids.MovingObject.call(this, pos, speed, direction, radius, color);
 	}
 	Asteroid.inherits(Asteroids.MovingObject);
 
 	Asteroid.COLOR = "#ccc";
 	Asteroid.RADIUS = 40;
+
+	Asteroid.prototype.makePoints = function (numPoints) {
+		this.points = [];
+		var azimuths = [0, 2*Math.PI];
+
+		for (var i = 0; i <= numPoints; i++) {
+			azimuths.push(Math.random()*Math.PI*2);
+		};
+
+		azimuths = azimuths.sort();
+		for (var i = 0; i < azimuths.length; i++) {
+			az = azimuths[i];
+			var noise = Math.random()*this.radius*0.2;
+			var x = (this.radius + noise)* Math.cos(az);
+			var y = (this.radius + noise) * Math.sin(az);
+			this.points.push([x,y]);
+		};
+
+		return this.points;
+	}
 
 	Asteroid.randomAsteroid = function(dimX, dimY) {
 
@@ -23,7 +43,22 @@
 		var speed = random(2);
 		var direction = [randomDirection(2), randomDirection(2)];
 		var radius = random(Asteroid.RADIUS);
-		return new Asteroid(pos, speed, direction, radius, Asteroid.COLOR);
+		asteroid = new Asteroid(pos, speed, direction, radius, Asteroid.COLOR);
+		asteroid.makePoints(20);
+		return asteroid;
 	}
 
+	Asteroid.prototype.draw = function(ctx) {
+		ctx.beginPath()
+		ctx.lineWidth = 2;
+    ctx.lineJoin = 'round';
+		for (var i = 1; i < this.points.length - 1; i++) {
+			ctx.lineTo(this.points[i][0] + this.pos[0], this.points[i][1] + this.pos[1]);
+		};
+		ctx.fillStyle = this.color;
+		ctx.closePath();
+		ctx.fill();
+
+		ctx.stroke();
+	}
 })();
